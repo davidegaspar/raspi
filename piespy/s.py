a432966ebc57facf01caadbfdc14cb7d9287497d
davidegaspar <git@davidegaspar.com>
2016-10-08 22:12:44 +0100

    fix

--- piespy/s.py
+++ piespy/s.py
@@ -8,16 +8,22 @@ class DetectMotion(picamera.array.PiMotionAnalysis):
     def __init__(self, camera, stream):
         super(DetectMotion, self).__init__(camera)
         self.stream = stream
+        self.recording = False
     def analyze(self, a):
         a = np.sqrt(
             np.square(a['x'].astype(np.float)) +
             np.square(a['y'].astype(np.float))
             ).clip(0, 255).astype(np.uint8)
         if (a > 60).sum() > 10:# If there're more than 10 vectors with a magnitude greater than 60, then say we've detected motion
-            print('motion-%s' % datetime.datetime.now().isoformat())
-            camera.wait_recording(10) # Keep recording for 10 seconds
-            self.stream.copy_to('motion-%s.h264' % datetime.datetime.now().isoformat())
-            print('saved!')
+            print('.')
+            if !self.recording:
+                self.recording = True
+                filename = 'motion-%s.h264' % datetime.datetime.now().isoformat()
+                print('saving: %s' % filename)
+                camera.wait_recording(10) # Keep recording for 10 seconds
+                self.stream.copy_to(filename)
+                print('saved: %s' % filename)
+                self.recording = False
 
 camera = picamera.PiCamera()
 stream = picamera.PiCameraCircularIO(camera, seconds=20)
